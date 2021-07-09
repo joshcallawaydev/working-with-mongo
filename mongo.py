f5bbdfe839fe3f32409985525e5bb57a522b9894
joshcallawaydev <joshrcal89@gmail.com>
2021-07-09 19:35:29 +0000

    Adding data with insert. Single and multiple entries.

--- mongo.py
+++ mongo.py
@@ -20,44 +20,11 @@ def mongo_connect(url):
 
 conn = mongo_connect(MONGO_URI)
 
+
 coll = conn[DATABASE][COLLECTION]
 
-documents = coll.find()
 
-""" Insert a single document """
-# new_doc = {
-#     "first": "douglas",
-#     "last": "adams",
-#     "dob": "11/03/1952",
-#     "gender": "m",
-#     "hair_color": "grey",
-#     "occupation": "writer",
-#     "nationality": "british"
-# }
-# coll.insert(new_doc)
-
-""" Insert multipe documents """
-# new_docs = [{
-#     "first": "terry",
-#     "last": "pratchett",
-#     "dob": "28/04/1948",
-#     "gender": "m",
-#     "hair_color": "not much",
-#     "occupation": "writer",
-#     "nationality": "british"
-# }, {
-#     "first": "george",
-#     "last": "rr martin",
-#     "dob": "20/09/1948",
-#     "gender": "m",
-#     "hair_color": "white",
-#     "occupation": "writer",
-#     "nationality": "american"
-# }]
-# coll.insert_many(new_docs)
-
-""" Find documents with 'first' name set to 'douglas' """
-# documents = coll.find({"first": "douglas"})
+documents = coll.find({"first": "douglas"})
 
 """ Delete documents with 'first' name set to 'douglas' """
 # coll.remove({"first": "douglas"})
@@ -80,6 +47,8 @@ documents = coll.find()
 #     {"$set": {"hair_color": "maroon"}}
 # )
 # documents = coll.find({"nationality": "american"})
+
+# documents = coll.find()
 
 for doc in documents:
     print(doc)
